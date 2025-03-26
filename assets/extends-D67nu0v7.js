c6b05be1220e927586d57ba3e21026fc62da300f
Kanriver <Kannatee2546kan@gmail.com>
2025-03-27 03:59:32 +0700

    Updates

--- assets/extends-D67nu0v7.js
+++ assets/extends-D67nu0v7.js
@@ -1,4 +1,4 @@
-import{r as Ye,g as vx,j as Dn}from"./index-Bn2v4q8b.js";/**
+import{r as Ye,g as vx,j as Dn}from"./index-CysBkl7F.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
